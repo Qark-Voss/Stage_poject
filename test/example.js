b87b71c0671cc088141251684b8e369ac2cb2ff2
Giacomo Quadrio <gquadrio@gmail.com>
2012-05-15 14:35:24 +0200

    Preparato script per disegnare in HTML5

--- test/example.js
+++ test/example.js
@@ -1,8 +1,21 @@
-var canvasDiv = document.getWlementById("canvasDiv");
-canvas = document.createElement("canvas");
-canvas.setAttribute("width", canvasWidth);
-canvas.setAttribute("height", canvasHeight);
-canvas.setAttribute("id", "canvas");
+
+
+
+var canvasDiv = document.getWlementById('canvasDiv');
+
+var canvasWidth = 500;
+var canvasHeight = 500;
+
+canvas = document.createElement('canvas');
+canvas.setAttribute('width', canvasWidth);
+canvas.setAttribute('height', canvasHeight);
+canvas.setAttribute('id', 'canvas');
 canvasDiv.appendChild(canvas);
 
+if(typeof G_vmlCanvasManager != 'undefine') {
+	
+	canvas = G_vmlCanvasManager.initElement(canvas);
+	
 }
+
+context  = canvas.getContext("2d");
