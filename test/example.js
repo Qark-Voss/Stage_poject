cc0b8b8dcc5c8b3c54117271352eab950f8f4289
Giacomo Quadrio <gquadrio@gmail.com>
2012-05-15 11:07:00 +0200

    test app

---
+++ test/example.js
@@ -0,0 +1,8 @@
+var canvasDiv = document.getWlementById("canvasDiv");
+canvas = document.createElement("canvas");
+canvas.setAttribute("width", canvasWidth);
+canvas.setAttribute("height", canvasHeight);
+canvas.setAttribute("id", "canvas");
+canvasDiv.appendChild(canvas);
+
+}
