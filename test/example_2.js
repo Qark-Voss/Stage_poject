7ac6fb4d229f4a681763e7ad8506d2d2d94e68ff
Giacomo Quadrio <gquadrio@gmail.com>
2012-05-21 12:56:05 +0200

    Aggiunte funzionalità per selezionare strumento e anche caricare le immagini!

--- test/example_2.js
+++ test/example_2.js
@@ -2,20 +2,40 @@
 var canvasWidth = 500;
 var canvasHeight = 500;
 var curCol = "red";
+var precEraser = "red";
 var lineW = 5;
 var lineStyle = "round";
+var img = new Image();
     
 var CanvasDrawr = function(options) {
 
-    var canvasDiv = document.getElementById('canvasDiv');
-	var clearButton = document.getElementById('clear_canv');
+	
+    var canvasDiv = document.getElementById('canvasDiv'); // I'm importing the canvasDiv
+	var clearButton = document.getElementById('clear_canv'); // I'm getting the clear_canv button
+	
+	// Getting the tools
+	var pencilTool = document.getElementById('pencil');
+	var eraserTool = document.getElementById('eraser');
+	
+	// Getting the button to change color
 	var purpleColor = document.getElementById('purple_b');
 	var blackColor = document.getElementById('black_b');
-	var canvas = document.createElement('canvas');
+	
+	// Getting button to change size
 	var sizeSmall = document.getElementById('small');
 	var sizeMedium = document.getElementById('medium');
 	var sizeBig = document.getElementById('big');
+	
+	// Getting the button to load images
+	var loadImg = document.getElementById('load');
+	
+	// Creating a new element for teh HTML page: canvas
+	var canvas = document.createElement('canvas');
+	
+	// Appending to the HTML file the new element
 	canvasDiv.appendChild(canvas);
+	
+	
 	
 	if(typeof G_vmlCanvasManager != 'undefined') {
 	
@@ -24,12 +44,14 @@ var CanvasDrawr = function(options) {
 	}
 
     ctxt = canvas.getContext("2d");
-        
+    
+    // Setting the canvas sizes
     canvas.setAttribute('width', canvasWidth);
 	canvas.setAttribute('height', canvasHeight);
     canvas.width = canvas.offsetWidth;
     canvas.style.width = '';
 
+	// Setting the canvas style
     ctxt.lineWidth = lineW;
     ctxt.lineCap = lineStyle;
     ctxt.pX = undefined;
@@ -46,11 +68,14 @@ var CanvasDrawr = function(options) {
             canvas.addEventListener('touchstart', self.preDraw, false);
             canvas.addEventListener('touchmove', self.draw, false);
 			clearButton.addEventListener('mousedown' , self.clearCanvas, false);
-            purpleColor.addEventListener('mousedown' , self.purpleColor, false);
-			blackColor.addEventListener('mousedown', self.purpleColor, false);
+            purpleColor.addEventListener('mousedown' , self.changeColor, false);
+			blackColor.addEventListener('mousedown', self.changeColor, false);
 			sizeSmall.addEventListener('mousedown', self.changeSize, false);
 			sizeMedium.addEventListener('mousedown', self.changeSize, false);
 			sizeBig.addEventListener('mousedown', self.changeSize, false);
+			pencilTool.addEventListener('mousedown', self.selectTool, false);
+			eraserTool.addEventListener('mousedown', self.selectTool, false);
+			loadImg.addEventListener('mousedown', self.loadNewImg, false);
             
         },
 
@@ -58,10 +83,10 @@ var CanvasDrawr = function(options) {
 
             $.each(event.touches, function(i, touch) {
               
-                var id      = touch.identifier;
+                var id = touch.identifier;
               
-                lines[id] = { x     : this.pageX - offset.left, 
-                              y     : this.pageY - offset.top, 
+                lines[id] = { x : this.pageX - offset.left, 
+                              y : this.pageY - offset.top, 
                               color : curCol
                            };
             });
@@ -99,20 +124,39 @@ var CanvasDrawr = function(options) {
 
 		clearCanvas: function(event){
 			
-			ctxt.fillStyle = '#ffffff';
-			ctxt.fillRect(0, 0, canvasWidth, canvasHeight);
-						
-			canvas.width = canvas.width;
-
-			ctxt.lineWidth = lineW;
+			if(this.id == 'clear_canv'){
 			
-			ctxt.lineCap = lineStyle;
-		    
+				ctxt.fillStyle = '#ffffff';
+				ctxt.fillRect(0, 0, canvasWidth, canvasHeight);
+						
+				canvas.width = canvas.width;
+
+				ctxt.lineWidth = lineW;
+			
+				ctxt.lineCap = lineStyle;
+	    	}
 
 		},
 		
+		selectTool: function(event){
+			
+			if(this.id == 'pencil'){
+				
+				curCol = precEraser;
+				
+			}
+			
+			if(this.id == 'eraser'){
+				
+				precErase = curCol;
+				curCol = 'white';
+				
+			}
+			
+		},
 		
-		purpleColor: function(event){
+		
+		changeColor: function(event){
 						
 			if(this.id == 'purple_b'){
 				
@@ -155,6 +199,14 @@ var CanvasDrawr = function(options) {
 					
 				
 			
+		},
+		
+		loadNewImg: function(event){
+							
+				img.src = 'img/keroro.jpg';
+
+				ctxt.drawImage(img, 0, 0);
+
 		}
 		
 
